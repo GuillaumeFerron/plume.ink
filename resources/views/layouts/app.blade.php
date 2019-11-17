5c986ce5fc34792c6684abe8eeba112904cbdc5f
GuillaumeFerron <guillaume.ferron2@gmail.com>
2019-11-16 20:41:56 -0500

    Meta tags fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,17 +10,13 @@
     <title>{{ config('app.name', 'Plume.ink') }}</title>
     <!-- Primary Meta Tags -->
     <meta name="title" content="Plume.ink">
-    <meta name="description" content="Got something on your mind?
-Forget about overkill note-taking apps with way too many useless features and focus on what's important.
-Start writing now with Plume.ink !">
+    <meta name="description" content="Got something on your mind? Forget about overkill note-taking apps with way too many useless features and focus on what's important. Start writing now with Plume.ink !">
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="{{ config('app.url', 'Plume.ink') }}">
     <meta property="og:title" content="{{ config('app.name', 'Plume.ink') }}">
-    <meta property="og:description" content="Got something on your mind?
-Forget about overkill note-taking apps with way too many useless features and focus on what's important.
-Start writing now with Plume.ink !">
+    <meta property="og:description" content="Got something on your mind? Forget about overkill note-taking apps with way too many useless features and focus on what's important. Start writing now with Plume.ink !">
     <meta property="og:image"
           content="{{ asset('images/og_image.jpg') }}">
 
@@ -28,9 +24,7 @@ Start writing now with Plume.ink !">
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="{{ config('app.url', 'Plume.ink') }}">
     <meta property="twitter:title" content="{{ config('app.name', 'Plume.ink') }}">
-    <meta property="twitter:description" content="Got something on your mind?
-Forget about overkill note-taking apps with way too many useless features and focus on what's important.
-Start writing now with Plume.ink !">
+    <meta property="twitter:description" content="Got something on your mind? Forget about overkill note-taking apps with way too many useless features and focus on what's important. Start writing now with Plume.ink !">
     <meta property="twitter:image"
           content="{{ asset('images/og_image.jpg') }}">
 
