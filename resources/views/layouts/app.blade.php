4e7ca11f806b4477b23bb891ddafae251e42c8fb
GuillaumeFerron <guillaume.ferron2@gmail.com>
2019-11-16 20:22:45 -0500

    Og tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,32 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Plume') }}</title>
+    <title>{{ config('app.name', 'Plume.ink') }}</title>
+    <!-- Primary Meta Tags -->
+    <meta name="title" content="Plume.ink">
+    <meta name="description" content="Got something on your mind?
+Forget about overkill note-taking apps with way too many useless features and focus on what's important.
+Start writing now with Plume.ink !">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ config('app.url', 'Plume.ink') }}">
+    <meta property="og:title" content="{{ config('app.name', 'Plume.ink') }}">
+    <meta property="og:description" content="Got something on your mind?
+Forget about overkill note-taking apps with way too many useless features and focus on what's important.
+Start writing now with Plume.ink !">
+    <meta property="og:image"
+          content="{{ asset('images/og_image.jpg') }}">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="{{ config('app.url', 'Plume.ink') }}">
+    <meta property="twitter:title" content="{{ config('app.name', 'Plume.ink') }}">
+    <meta property="twitter:description" content="Got something on your mind?
+Forget about overkill note-taking apps with way too many useless features and focus on what's important.
+Start writing now with Plume.ink !">
+    <meta property="twitter:image"
+          content="{{ asset('images/og_image.jpg') }}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,57 +50,6 @@
 </head>
 <body>
 <div id="app">
-    {{--        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">--}}
-    {{--            <div class="container">--}}
-    {{--                <a class="navbar-brand" href="{{ url('/') }}">--}}
-    {{--                    {{ config('app.name', 'Plume') }}--}}
-    {{--                </a>--}}
-    {{--                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
-    {{--                    <span class="navbar-toggler-icon"></span>--}}
-    {{--                </button>--}}
-
-    {{--                <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-    {{--                    <!-- Left Side Of Navbar -->--}}
-    {{--                    <ul class="navbar-nav mr-auto">--}}
-
-    {{--                    </ul>--}}
-
-    {{--                    <!-- Right Side Of Navbar -->--}}
-    {{--                    <ul class="navbar-nav ml-auto">--}}
-    {{--                        <!-- Authentication Links -->--}}
-    {{--                        @guest--}}
-    {{--                            <li class="nav-item">--}}
-    {{--                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
-    {{--                            </li>--}}
-    {{--                            @if (Route::has('register'))--}}
-    {{--                                <li class="nav-item">--}}
-    {{--                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
-    {{--                                </li>--}}
-    {{--                            @endif--}}
-    {{--                        @else--}}
-    {{--                            <li class="nav-item dropdown">--}}
-    {{--                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
-    {{--                                    {{ Auth::user()->name }} <span class="caret"></span>--}}
-    {{--                                </a>--}}
-
-    {{--                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
-    {{--                                    <a class="dropdown-item" href="{{ route('logout') }}"--}}
-    {{--                                       onclick="event.preventDefault();--}}
-    {{--                                                     document.getElementById('logout-form').submit();">--}}
-    {{--                                        {{ __('Logout') }}--}}
-    {{--                                    </a>--}}
-
-    {{--                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
-    {{--                                        @csrf--}}
-    {{--                                    </form>--}}
-    {{--                                </div>--}}
-    {{--                            </li>--}}
-    {{--                        @endguest--}}
-    {{--                    </ul>--}}
-    {{--                </div>--}}
-    {{--            </div>--}}
-    {{--        </nav>--}}
-
     <main>
         <div class="row m-0 pt-3 log-buttons">
             @if(Route::current()->getName() === 'login')
